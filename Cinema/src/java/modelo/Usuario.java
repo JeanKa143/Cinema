628babb116409e9ff071a7afc3f2d6b8d8a03e66
Diana Fallas <65523878+dianattrix@users.noreply.github.com>
2021-06-11 23:35:12 -0600

    Update Usuario.java

--- Cinema/src/java/modelo/Usuario.java
+++ Cinema/src/java/modelo/Usuario.java
@@ -11,4 +11,48 @@ package modelo;
 
 public class Usuario {
     
+    private String id;
+    private String clave;
+    
+    public enum Rol {
+        administrador,
+        cliente
+    }
+    
+    public Rol rol; 
+    
+    public Usuario(){
+        
+    }
+    
+    public Usuario(String id, String clave, Rol rol){
+        this.id = id;
+        this.clave = clave;
+        this.rol = rol;
+    }
+    
+    public void setRol(Rol rol){
+        this.rol = rol;
+    }
+    
+    public void setClave(String clave){
+        this.clave = clave;
+    }
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public Rol getRol(){
+        return this.rol;   
+    }
+    
+    public String getId(){
+        return this.id;
+    }
+    
+    public String getClave(){
+        return this.clave;
+    }
+   
 }
