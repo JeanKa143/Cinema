104a87a733c18fde7f0dbe16f28eb439bcee6d8a
Diana Fallas <65523878+dianattrix@users.noreply.github.com>
2021-06-27 18:06:28 -0600

    Delete procederCompra.js

--- Cinema/web/js/procederCompra.js
+++
@@ -1,9 +0,0 @@
-/* 
- * Integrantes del equipo:
- * - Luis Fallas Carvajal                   Cedula: 305180995
- * - Diana Fallas Mendez                Cedula: 117770654
- * - Jean Carlo Jimenez Oviedo     Cedula: 402500956
- * 
- * Curso: EIF209 ProgramacionIV
- * I Ciclo 2021
- */
